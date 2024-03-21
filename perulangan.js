9feb7867f10453f1216745bb7414c0d6a89a886d
RACHUL INDRA <92237605+Luhcar@users.noreply.github.com>
2024-03-21 11:53:52 +0700

    Add files via upload

---
+++ perulangan.js
@@ -0,0 +1,21 @@
+/*
+PERULANGAN JAVASCRIPT 
+1. for, for of, for in, foreach
+2. while, do while
+*/
+
+for(let x = 1; x <= 10; x++){
+        console.log('Data x ke - ', x)
+}
+
+let y = 1
+while(y <= 10) {
+    console.log('Data y ke - ', y)
+    y++
+}
+
+let z = 30
+do{
+    console.log('Data z ke - ', z)
+    z++
+}while (z <= 10)
